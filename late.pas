8350b3afed1254fadd5cada526006560e7f8241e
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-07 17:50:13 +0900

    Update late.pas

--- late.pas
+++ late.pas
@@ -1,12 +1,14 @@
 //ABC177_A late
 
-program xxx;
 var
   D,T,S: Integer;
+  
 begin
   readln(D,T,S);
+  
 if D<=T*S then
     writeln('Yes')
 else
-    writeln('No');   
+    writeln('No'); 
+    
 end.
