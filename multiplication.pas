6266a21cc3a74b33b536057cfb386e448c66371f
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-02 17:02:09 +0900

    Create multiplication.pas

---
+++ multiplication.pas
@@ -0,0 +1,41 @@
+//abc169_a multiplication.pas
+################################
+################################
+################################
+program main;
+var a,b : integer;
+begin 
+  read(a,b);
+  writeln(a*b);
+end.
+################################
+program hi;
+var a,b: integer;
+begin
+    readln(a,b);
+    writeln(a*b);
+end.
+################################
+program ex(input, output);
+var
+  A, B: Integer;
+begin
+  Readln(A, B);
+  Writeln(A*B);
+  Readln;
+end.
+################################
+var a, b:longint;
+BEGIN
+    readln(a,b);
+    writeln(a*b);
+END.
+################################
+program main;
+var
+  a,b : Integer;
+begin
+  readln(a,b);
+  writeln(a*b);
+end.
+################################
