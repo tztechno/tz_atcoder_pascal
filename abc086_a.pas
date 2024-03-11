b5f2a6d67a8b69cc0624a6c27a9ea7fea9e55dfa
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-11 21:54:24 +0900

    Create abc086_a.pas

---
+++ abc086_a.pas
@@ -0,0 +1,41 @@
+//abc086_a.pas
+################################
+################################
+################################
+var a,b:Longint;
+begin
+read(a,b);
+if a*b mod 2=0 then writeln('Even')else writeln('Odd');
+end.
+################################
+var a,b :Integer;
+begin
+readln(a,b);
+if (a*b) mod 2=0 then
+ writeln(' Even')
+ else writeln(' Odd');
+end.
+################################
+program ex(input, output);
+var
+  A, B: Integer;
+begin
+  Readln(A, B);
+  if (A*B mod 2 = 0) then
+    Writeln('Even')
+  else
+    Writeln('Odd');
+  Readln;
+end.
+################################
+var
+  a, b, c: Integer;
+begin
+  readln(a, b);
+  c := (a * b) mod 2;
+  if c = 0 then
+    writeln('Even')
+  else
+    writeln('Odd');
+end.
+################################
