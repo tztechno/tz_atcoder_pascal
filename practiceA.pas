9b12d3aa2db6d4e53ad94bc806aeaf5971809523
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-14 01:32:19 +0900

    Create practiceA.pas

---
+++ practiceA.pas
@@ -0,0 +1,55 @@
+//practiceA.pas
+##################################
+1
+2 3
+test
+##################################
+##################################
+##################################
+##################################
+##################################
+##################################
+##################################
+##################################
+##################################
+var
+	a, b, c: Integer;
+    s: String[100];
+
+begin
+	readln(a);
+    read(b);
+    readln(c);
+    readln(s);
+    writeln(a + b + c, ' ' , s);
+    
+end.
+##################################
+program a(intput, output);
+uses
+  SysUtils;
+var
+  A, B, C: Integer;
+  S: String;
+begin
+  Readln(A, B, C);
+  Readln(S);
+  Writeln(Format('%d %s', [A+B+C, S]));
+  Readln;
+end.
+##################################
+// program hello(output);
+var
+  a, b, c : integer;
+  s : ShortString; // 文字列型は？
+begin
+  // test
+  // writeln('hello, world')   { 文字列を出力 }
+  readln(a);
+  read(b);
+  readln(c);
+  readln(s);
+  
+  writeln(a+b+c, ' ', s);
+end.
+##################################
